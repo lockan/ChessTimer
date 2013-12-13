275a610304da31119e16758de2a10d5e5974074e
lockan <lockan@gmail.com>
2013-12-12 20:40:08 -0800

    Test: Adding source files to project.

---
+++ src/ChessTimer.java
@@ -0,0 +1,21 @@
+public class ChessTimer {
+
+	public static void main(String[] args) {
+		
+		long start = 1000000;
+		long end = 0;
+		
+		System.out.println("Start: " + start);
+		
+		for (long i = start; i > end; i--)
+		{
+			start--;
+		}
+		
+		long stop = System.currentTimeMillis();
+		System.out.println("Stop: " + start);
+
+	
+	}
+
+}
