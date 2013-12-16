c0a26265851e6df0c796942ac4c75931db50c855
lockan <lockan@gmail.com>
2013-12-16 13:37:32 -0800

    Countdown timer - first pass. Works for one second, then thread stops. Will continue to run in debug if breakpoint is set immediately after the schedule call.

--- src/ChessTimer.java
+++ src/ChessTimer.java
@@ -1,23 +1,32 @@
-public class ChessTimer {
+import java.util.Timer;
+import java.util.TimerTask;
 
+public class ChessTimer {
+	
+	private long t_time = 0;
+	private long t_remain = 0;
+	private final long updateFreq = 1000;
+	private CountDownTask cTask = new CountDownTask();		
+	
+	private class CountDownTask extends TimerTask {
+        public void run() {
+            t_remain--;
+            System.out.println(t_remain);
+        }
+    }
+	
 	public static void main(String[] args) {
 		
-		long start = 1000000;
-		long end = 0;
-		
-		System.out.println("Start: " + start);
-		
-		for (long i = start; i > end; i--)
-		{
-			start--;
-		}
-		
-		long stop = System.currentTimeMillis();
-		System.out.println("Stop: " + start);
-
-		System.out.println("Pushed from hackbox");
-
-	
+		ChessTimer ct = new ChessTimer();
+		ct.initTimer(1000000);
+				
+		Timer countDown = new Timer("Game Clock", true);	//isDaemon - for repeated tasks. 		
+		countDown.scheduleAtFixedRate(ct.cTask, 0, ct.updateFreq);
 	}
-
+	
+	public void initTimer(long inTime) {
+		t_time = inTime;
+		t_remain = t_time;
+		System.out.println("Timer init to " + t_time);
+	}
 }
